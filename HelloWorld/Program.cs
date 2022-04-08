5c79e11c403b98c3b41a9f0ceb47f25ae654390a
Nhujarski <52231438+Nhujarski@users.noreply.github.com>
2022-04-08 13:11:02 -0700

    first c sharp program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  public class Program
+    {
+       //entry point of program
+        public static void Main(string[] args)
+        {
+         int age = 38; // dclaring a varible type integer
+            Console.WriteLine("Hi Im Nick and Im " + age);
+        }
+    }
+}
